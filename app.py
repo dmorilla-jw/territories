655e273113c1e902caf20e0b35402ee8d491b810
dmorilla-jw <dmorilla.jw@gmail.com>
2026-04-23 11:35:08 -0700

    Update app.py

--- app.py
+++ app.py
@@ -262,5 +262,8 @@ def territory_pdf(territory_id):
     response.headers["Content-Disposition"] = f'inline; filename="{t["id"]}.pdf"'
     return response
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
